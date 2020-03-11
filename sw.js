3f2dbe97a82b672d6b09234b397df6072477a2f1
Gnaut <goran.koren@studionaut.com>
2020-03-11 19:59:08 +0100

    fix

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('push', function(e) {
+    var options = {
+        body: 'Push notifikacija gnereirana iz push.js',
+        icon: 'images/example.png',
+        vibrate: [100, 50, 100],
+        data:{
+            dateOfArrival: Date.now(),
+            primaryKey: '2'
+        },
+        actions:[
+            {action:'explore', title: 'Yoga začne danes čez 1 uro',
+            icon: 'images/checkmark.png'},
+            {action: 'close', title: 'Close',
+            icon: 'images/xmark.png'},
+        ]
+    };
+    e.waitUntil(
+        sečf-ServiceWorkerRegistration.showNotification('Živio', options)
+    );
+
+})
